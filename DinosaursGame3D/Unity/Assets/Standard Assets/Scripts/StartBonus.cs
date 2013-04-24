55ce8b5f1444e12e71037a3390594f672f879a13
Brittany Oswald <brittany.oswald@gmail.com>
2013-04-24 09:26:05 -0700

    gets desired target position of the camera destination at the start if the bonus

--- DinosaursGame3D/Unity/Assets/Standard Assets/Scripts/StartBonus.cs
+++ DinosaursGame3D/Unity/Assets/Standard Assets/Scripts/StartBonus.cs
@@ -8,28 +8,47 @@ public class StartBonus : MonoBehaviour {
 	bool closeEnough;
 	public float tolerance = 0.1f;
 	Vector3 destinationPos;
-	Vector3 destinationRot;
+	Quaternion destinationRot;
+	Transform defaultTransform;
 	
 	// Use this for initialization
 	void Start () {
 		closeEnough = true;
-		destinationPos = transform.position;
-		destinationRot = transform.rotation;
+		defaultTransform = transform;
+		destinationPos = defaultTransform.position;
+		destinationRot = defaultTransform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		closeEnough = Vector3.Distance(Camera.mainCamera.transform.position, destinationPos) < tolerance;
+		if (!closeEnough) {
+			//Camera.mainCamera.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+			//Camera.mainCamera.transform.
+		}
 		
 	}
 	
 	void OnTriggerEnter(Collider other) {
         if(other.gameObject == player) {
-			//Camera thirdPersonView = player.transform.position - player.transform.forward;
-			//Camera.mainCamera.transform.Translate(thirdPersonView);//Vector3.forward);
-			GameObject thirdPersonCamera = player.transform.Find("ThirdPersonCamera");
-			destinationPos.position = thirdPersonCamera.transform.position;
+			Vector3 thirdPersonView = player.transform.Find("ThirdPersonCamera").transform.position; //player.transform.position - player.transform.forward;
+			//DebugStreamer.message = ("thirdPersonView :" + thirdPersonView);
+			Debug.Log("thirdPersonView :" + thirdPersonView);
+			if(thirdPersonView == null){
+				Camera.mainCamera.transform.Translate(thirdPersonView);//Vector3.forward * 40);
+			}
+			else {
+				Camera.mainCamera.transform.TransformDirection(thirdPersonView);//Vector3.forward);
+			}
+			
+			//Camera.mainCamera.transform.Translate(Vector3.forward * 40);
+			/*closeEnough = false;
+			
+			// Set the destination.
+			Transform thirdPersonCamera = player.transform.Find("ThirdPersonCamera");
+			destinationPos = thirdPersonCamera.transform.position;
 			destinationRot = thirdPersonCamera.transform.rotation;
+			*/
 		}
     }
 }
